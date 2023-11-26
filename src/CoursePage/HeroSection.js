28e2d0c2d08d7c25a8e5d802de48a0cf33aa2581
navjot369 <personal.navjotsingh@gmail.com>
2023-11-26 19:40:33 +0530

    Removed >>>>>>>

--- src/CoursePage/HeroSection.js
+++ src/CoursePage/HeroSection.js
@@ -2,10 +2,6 @@ import React from 'react';
 import video1 from './../Images/SampleVid1.mp4';
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3d8e3fc (Add course landing page features)
 export default function Section1() {
     return (<div className="heroSectionCont">
         <div className="sec1Left">
@@ -16,17 +12,8 @@ export default function Section1() {
             <div className="CourseRegisCont"><Link to={'/'} className="RegisLink">Register Now</Link></div>
         </div>
         <div className="sec1Right">
-<<<<<<< HEAD
-            <video src={video1} className="Video1" autoPlay loop muted/>
-=======
             <video src={video1} className="Video1" alt="Intention ShowCase" autoPlay loop muted/>
->>>>>>> 3d8e3fc (Add course landing page features)
         </div>
     </div>);
 }
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 3d8e3fc (Add course landing page features)
