798aba83014ff4f38dd673c1dd0091b3018cbcda
navjot369 <personal.navjotsingh@gmail.com>
2023-11-26 19:39:01 +0530

    Type Mistake fix

--- src/CoursePage/course.js
+++ src/CoursePage/course.js
@@ -1,10 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import { AppLogo, UserAccount } from './../navbar.js';
-import { NavMenu } from './courseNavbar.js';
-import Section1 from './HeroSection.js';
-import './course.css';
-=======
 import './course.css';
 import { AppLogo, UserAccount } from './../navbar.js';
 import { NavMenu } from './courseNavbar.js';
@@ -15,7 +9,6 @@ import HiringSec from './HiringSection.js';
 import SpeakerSec from './SpeakerSec.js';
 import { Footer } from './../Sections.js';
 
->>>>>>> 3d8e3fc (Add course landing page features)
 
 function Course() {
     return (<div>
@@ -23,14 +16,11 @@ function Course() {
         <UserAccount isLogin=""/>
         <NavMenu />
         <Section1 />
-<<<<<<< HEAD
-=======
         <UspSection />
         <Features />
         <HiringSec />
         <SpeakerSec />
         <Footer />
->>>>>>> 3d8e3fc (Add course landing page features)
     </div>);
 }
 
