631b54a410cb47487bea23466ae773258bf51f99
navjot369 <personal.navjotsingh@gmail.com>
2023-12-07 19:32:00 +0530

    Add feature section content in course page

--- src/CoursePage/FeaturesSec.js
+++ src/CoursePage/FeaturesSec.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import icon1 from './../Images/automation.png';
+import icon2 from './../Images/project.png';
+import icon3 from './../Images/webinar.png';
+import icon4 from './../Images/reading-book.png';
 
 export default function Features() {
     return(<div>
-        <div className="featureTagline">Where Data Science : Gets You Placed</div>
+        <div className="featureTagline">Where Data Science: <span className="getPlaced">Gets You Placed</span></div>
         <div className="featureCont">
-            <FeatureBox cont="Industry Proven Curricullum" />
-            <FeatureBox cont="Real-Life Projects" />
-            <FeatureBox cont="Live Classes" />
-            <FeatureBox cont="Flexible Learning" />
+            <FeatureBox icon={icon1} cont="Industry Proven Curricullum" />
+            <FeatureBox icon={icon2} cont="Real-Life Projects" />
+            <FeatureBox icon={icon3} cont="Live Classes" />
+            <FeatureBox icon={icon4} cont="Flexible Learning" />
         </div>
     </div>);
 }
 
 function FeatureBox(props) {
     return(<div className="FeatBox">
+        <img src={props.icon} alt="Match" className="feature-icon" />
         <p className="featureLine">{props.cont}</p>
     </div>);
 }
